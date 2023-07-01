93f3c10528ac7641af6b7b37abd1274b1df59641
liu-jinxii <arny1371621210@163.com>
2023-07-01 23:36:21 +0800

    MusicPlayer

--- MusicPlayer/src/main/java/com/example/demo/model/Constant.java
+++ MusicPlayer/src/main/java/com/example/demo/model/Constant.java
@@ -6,7 +6,8 @@ package com.example.demo.model;
 public class Constant {
     public static final String SESSION_USERINFO_KEY = "session_userinfo_key";
     //本地音乐存放路径
-    //public static final String MUSIC_FILE_PATH = "D:/LocalMusic/";
+    public static final String MUSIC_FILE_PATH = "D:/LocalMusic/";
+
     //服务器存放音乐路径(打包前记得改)
-    public static final String MUSIC_FILE_PATH = "/home/musicplayer/music/";
+    //public static final String MUSIC_FILE_PATH = "/home/musicplayer/music/";
 }
