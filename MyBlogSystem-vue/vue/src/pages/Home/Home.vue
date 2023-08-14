0db16807ae70ecc07dad64bd256958bf25e23486
liu-jinxii <arny1371621210@163.com>
2023-08-14 15:59:57 +0800

    mycnblog-vue

--- MyBlogSystem-vue/vue/src/pages/Home/Home.vue
+++ MyBlogSystem-vue/vue/src/pages/Home/Home.vue
@@ -18,27 +18,14 @@ export default {
 <template>
   <div id="home">
     <div class="wrapper">
+      <!-- 左侧边栏杆  -->
       <LeftSide/>
       <div class="main-container">
+        <!--  顶部栏    -->
         <Header/>
         <div class="user-box first-box">
-          <div class="account-wrapper" style="--delay: .8s">
-            <div class="account-profile">
-              <img src="https://images.unsplash.com/photo-1550314124-301ca0b773ae?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2215&q=80" alt="">
-              <div class="blob-wrap">
-                <div class="blob"></div>
-                <div class="blob"></div>
-                <div class="blob"></div>
-              </div>
-              <div class="account-name">Mike J Morgan</div>
-              <div class="account-title">Taxi Driver</div>
-            </div>
-            <div class="account card">
-              <div class="account-cash">$ 5637.04</div>
-              <div class="account-income">Total Income</div>
-              <div class="account-iban">**** **** **** 3060</div>
-            </div>
-          </div>
+          <!--   用户区域      -->
+          <User/>
           <div class="activity card" style="--delay: .2s">
             <div class="title">User Activities</div>
             <div class="subtitle">Hoo - is an adaptive Online Courses Application with a wide range of course directions. The students will have a great possibility to study.</div>
@@ -82,17 +69,12 @@ export default {
             </div>
           </div>
         </div>
-
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-@import url("https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500&display=swap");
-:root {
-  --delay: 0s;
-}
 
 * {
   outline: none;
@@ -115,7 +97,6 @@ body {
 }
 
 .wrapper {
-  max-width: 1600px;
   background-color: #0e0e23;
   display: flex;
   height: 100vh;
@@ -144,6 +125,7 @@ body {
 .user-box .cards-view svg {
   width: 24px;
 }
+
 .user-box .cards-header .title,
 .user-box .cards-view .title {
   margin: 0 16px;
@@ -319,8 +301,6 @@ body {
   margin-top: 4px;
 }
 
-
-
 @keyframes pieChart {
   100% {
     stroke-dashoffset: 0;
@@ -337,116 +317,6 @@ body {
 .degree svg {
   width: 24px;
   margin-right: 12px;
-}
-
-.
-
-.account {
-  width: 100%;
-  height: 180px;
-  margin-top: auto;
-  flex-grow: 0;
-  position: relative;
-  transition: 0.3s;
-  cursor: pointer;
-  transition: 0.3s;
-}
-.account:hover {
-  transform: scale(1.02);
-}
-.account:before {
-  content: "";
-  position: absolute;
-  width: 24px;
-  height: 24px;
-  box-shadow: -15px 0 0 0 #ef8741;
-  background: #ef415c;
-  top: 20px;
-  left: 42px;
-  border-radius: 50%;
-}
-.account-wrapper {
-  max-width: 310px;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  margin-left: 20px;
-  align-items: center;
-}
-.account-profile {
-  margin: auto;
-  position: relative;
-  text-align: center;
-  position: relative;
-}
-.account-profile img {
-  width: 84px;
-  height: 84px;
-  border-radius: 50%;
-  object-fit: cover;
-  object-position: left;
-  border: 3px solid #4255d3;
-  padding: 5px;
-}
-.account-profile .blob {
-  position: absolute;
-  border-radius: 50%;
-  animation: fly 5.8s linear infinite alternate;
-}
-.account-profile .blob:nth-child(1) {
-  width: 14px;
-  height: 14px;
-  top: 25px;
-  left: -20px;
-  background: #28327a;
-  animation-delay: 0.9s;
-}
-.account-profile .blob:nth-child(2) {
-  width: 18px;
-  height: 18px;
-  background: #87344c;
-  right: -20px;
-  top: -20px;
-  animation-delay: 0.2s;
-}
-.account-profile .blob:nth-child(3) {
-  width: 12px;
-  height: 12px;
-  background: #13645b;
-  right: -35px;
-  top: 50%;
-  animation-delay: 1.8s;
-}
-.account-name {
-  margin: 20px 0 10px;
-}
-.account-title {
-  font-size: 14px;
-}
-.account-cash {
-  font-size: 22px;
-  font-weight: 500;
-  margin-bottom: 6px;
-  padding-top: 16px;
-  position: relative;
-}
-.account-cash:before {
-  content: "";
-  position: absolute;
-  width: 5px;
-  height: 5px;
-  background: #9b9ca7;
-  right: 10px;
-  border-radius: 50%;
-  box-shadow: -10px 0 0 0 #9b9ca7, 10px 0 0 0 #9b9ca7;
-  top: 24px;
-}
-.account-income {
-  font-size: 14px;
-}
-.account-iban {
-  margin-top: auto;
-  font-weight: 500;
 }
 
 .table .status svg {
@@ -560,48 +430,6 @@ body {
   }
 }
 
-@media screen and (max-width: 1060px) {
-  .user-info .button,
-  .user-info .hour {
-    display: none;
-  }
-}
-
-@media screen and (max-width: 930px) {
-
-  .header-link {
-    display: none;
-  }
-
-  .user-info .profile {
-    margin-right: 0;
-  }
-}
-@media screen and (max-width: 850px) {
-
-  .user-box .discount {
-    margin-left: 0;
-    margin-top: 20px;
-  }
-
-  .left-side {
-    display: none;
-  }
-
-  .main-container {
-    padding-left: 25px;
-  }
-
-  .activity-links,
-  .destination {
-    margin-top: 20px;
-  }
-}
-@media screen and (max-width: 720px) {
-  .first-box .account-wrapper {
-    display: none;
-  }
-}
 @media screen and (max-width: 420px) {
   .destination-card + .destination-card {
     display: none;
--- MyBlogSystem-vue/vue/src/pages/HomeUser/User.vue
+++ MyBlogSystem-vue/vue/src/pages/HomeUser/User.vue
@@ -7,9 +7,157 @@ export default {
 </script>
 
 <template>
-
+  <div class="account-wrapper" style="--delay: .8s">
+    <div class="account-profile">
+      <img src="https://images.unsplash.com/photo-1550314124-301ca0b773ae?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2215&q=80" alt="">
+      <div class="blob-wrap">
+        <div class="blob"></div>
+        <div class="blob"></div>
+        <div class="blob"></div>
+      </div>
+      <div class="account-name">Mike J Morgan</div>
+      <div class="account-title">Taxi Driver</div>
+    </div>
+    <div class="account card">
+      <div class="account-cash">$ 5637.04</div>
+      <div class="account-income">Total Income</div>
+      <div class="account-iban">**** **** **** 3060</div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
+
+.account {
+  width: 100%;
+  height: 180px;
+  margin-top: auto;
+  flex-grow: 0;
+  position: relative;
+  transition: 0.3s;
+  cursor: pointer;
+
+}
+.account:hover {
+  transform: scale(1.02);
+}
+.account:before {
+  content: "";
+  position: absolute;
+  width: 24px;
+  height: 24px;
+  box-shadow: -15px 0 0 0 #ef8741;
+  background: #ef415c;
+  top: 20px;
+  left: 42px;
+  border-radius: 50%;
+}
+.account-wrapper {
+  max-width: 310px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-left: 20px;
+  align-items: center;
+}
+.account-profile {
+  margin: auto;
+  position: relative;
+  text-align: center;
+
+}
+.account-profile img {
+  width: 84px;
+  height: 84px;
+  border-radius: 50%;
+  object-fit: cover;
+  object-position: left;
+  border: 3px solid #4255d3;
+  padding: 5px;
+}
+.account-profile .blob {
+  position: absolute;
+  border-radius: 50%;
+  animation: fly 5.8s linear infinite alternate;
+}
+.account-profile .blob:nth-child(1) {
+  width: 14px;
+  height: 14px;
+  top: 25px;
+  left: -20px;
+  background: #28327a;
+  animation-delay: 0.9s;
+}
+.account-profile .blob:nth-child(2) {
+  width: 18px;
+  height: 18px;
+  background: #87344c;
+  right: -20px;
+  top: -20px;
+  animation-delay: 0.2s;
+}
+.account-profile .blob:nth-child(3) {
+  width: 12px;
+  height: 12px;
+  background: #13645b;
+  right: -35px;
+  top: 50%;
+  animation-delay: 1.8s;
+}
+.account-name {
+  margin: 20px 0 10px;
+}
+.account-title {
+  font-size: 14px;
+}
+.account-cash {
+  font-size: 22px;
+  font-weight: 500;
+  margin-bottom: 6px;
+  padding-top: 16px;
+  position: relative;
+}
+.account-cash:before {
+  content: "";
+  position: absolute;
+  width: 5px;
+  height: 5px;
+  background: #9b9ca7;
+  right: 10px;
+  border-radius: 50%;
+  box-shadow: -10px 0 0 0 #9b9ca7, 10px 0 0 0 #9b9ca7;
+  top: 24px;
+}
+.account-income {
+  font-size: 14px;
+}
+.account-iban {
+  margin-top: auto;
+  font-weight: 500;
+}
+
+.card {
+  background: #1a2049;
+  background: radial-gradient(circle, #1a2049 0%, #13162f 100%);
+  padding: 40px 30px;
+  border-radius: 6px;
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  max-height: 430px;
+  width: 100%;
+}
+.card .title {
+  font-size: 16px;
+  font-weight: 500;
+}
+.card .subtitle {
+  font-size: 13px;
+  line-height: 1.6em;
+}
+.card + .card {
+  margin-left: 20px;
+}
+
 
 </style>
