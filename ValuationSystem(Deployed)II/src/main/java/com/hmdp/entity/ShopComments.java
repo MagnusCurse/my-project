6fab0e0f3dc304e90323c9cb015dfd2b9302de19
liu-jinxii <arny1371621210@163.com>
2023-11-04 11:10:18 +0800

    valuation-sys

---
+++ ValuationSystem(Deployed)II/src/main/java/com/hmdp/entity/ShopComments.java
@@ -0,0 +1,65 @@
+package com.hmdp.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@TableName("tb_shop_comments")
+public class ShopComments implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 主键
+     */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 用户 id
+     */
+    private Long userId;
+
+    /**
+     * 探店 id
+     */
+    private Long shopId;
+
+    /**
+     * 回复的内容
+     */
+    private String content;
+    /**
+     * 星级评分
+     */
+    private Integer stars;
+
+    /**
+     * 点赞数
+     */
+    private Integer liked;
+
+    /**
+     * 状态，0：正常，1：被举报，2：禁止查看
+     */
+    private Boolean status;
+
+    /**
+     * 创建时间
+     */
+    private LocalDateTime createTime;
+
+    /**
+     * 更新时间
+     */
+    private LocalDateTime updateTime;
+}
