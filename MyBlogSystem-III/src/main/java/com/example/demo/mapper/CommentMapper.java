c0f9f76cc6993d9476e9e2c8e4bd46162709f29f
liu-jinxii <arny1371621210@163.com>
2023-11-06 20:06:17 +0800

    myblog-sys

--- MyBlogSystem-III/src/main/java/com/example/demo/mapper/CommentMapper.java
+++ MyBlogSystem-III/src/main/java/com/example/demo/mapper/CommentMapper.java
@@ -10,18 +10,18 @@ import java.util.List;
 
 @Mapper
 public interface CommentMapper extends BaseMapper<Comment> {
-    public int post(@Param("user_id") Integer user_id,
+     int post(@Param("user_id") Integer user_id,
                     @Param("blog_id") Integer blog_id,
                     @Param("username") String username,
                     @Param("comment") String comment);
 
-    public int reply(@Param("parent_id") Integer parent_id,
+     int reply(@Param("parent_id") Integer parent_id,
                      @Param("user_id") Integer user_id,
                      @Param("blog_id") Integer blog_id,
                      @Param("username") String username,
                      @Param("comment") String comment);
 
-    public int replyChildComment(@Param("parent_id") Integer parent_id,
+     int replyChildComment(@Param("parent_id") Integer parent_id,
                                  @Param("user_id") Integer user_id,
                                  @Param("blog_id") Integer blog_id,
                                  @Param("username") String username,
@@ -29,11 +29,11 @@ public interface CommentMapper extends BaseMapper<Comment> {
                                  @Param("replied_username") String replied_username);
 
 
-    public List<Comment> initParentComment(@Param("blog_id") Integer blog_id);
+     List<Comment> initParentComment(@Param("blog_id") Integer blog_id);
 
-    public List<Comment> initChildComment(@Param("parent_id") Integer parent_id,@Param("blog_id") Integer blog_id);
+     List<Comment> initChildComment(@Param("parent_id") Integer parent_id,@Param("blog_id") Integer blog_id);
 
-    public int deleteComment(@Param("id") Integer id);
+     int deleteComment(@Param("id") Integer id);
 
-    public int deleteChildComment(@Param("id") Integer id);
+     int deleteChildComment(@Param("id") Integer id);
 }
