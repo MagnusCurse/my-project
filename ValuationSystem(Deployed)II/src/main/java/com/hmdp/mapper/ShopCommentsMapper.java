e2dc122b60356677016aa5b6849720960bac862e
liu-jinxii <arny1371621210@163.com>
2023-11-06 19:55:23 +0800

    valuation-sys

--- ValuationSystem(Deployed)II/src/main/java/com/hmdp/mapper/ShopCommentsMapper.java
+++
@@ -1,9 +0,0 @@
-package com.hmdp.mapper;
-
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.hmdp.entity.ShopComments;
-
-public interface ShopCommentsMapper extends BaseMapper<ShopComments> {
-
-}
