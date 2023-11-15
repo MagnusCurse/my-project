946989db00628c19708e3bd045d2488ce97210b2
liu-jinxii <arny1371621210@163.com>
2023-11-15 11:44:30 +0800

    mycnblog-auto-test

--- MyBlogSystemAutoTest/src/test/java/com/blogWebAutoTest/Tests/BlogLoginTest.java
+++ MyBlogSystemAutoTest/src/test/java/com/blogWebAutoTest/Tests/BlogLoginTest.java
@@ -1,15 +1,17 @@
 package com.blogWebAutoTest.Tests;
 
 import com.blogWebAutoTest.utils.AutoTestUtils;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class BlogLoginTest {
     public static ChromeDriver chromeDriver = AutoTestUtils.createDriver();
 
@@ -27,6 +29,7 @@ public class BlogLoginTest {
      * 检查登录页面是否打开正确
      * 检查登录，注册，忘记密码选项是否存在
      */
+    @Order(1)
     @Test
     public void loginPageLoad() {
         // 检查登录标签
@@ -40,16 +43,47 @@ public class BlogLoginTest {
     /**
      * 检查能否正常登录
      */
+    @Order(2)
     @ParameterizedTest
     @CsvSource({"UserC1,1234","UserC2,5678"})
-    public void loginSuccess(String username, String password) {
-        // 执行登录执行步骤
+    public void loginSuccess(String username, String password) throws InterruptedException {
+        /* TODO 执行登录执行步骤 */
+        // 插入数据前要清空，否则自动化会报错
+        chromeDriver.findElement(By.cssSelector("body > div > div > form > div:nth-child(1) > input[type=text]")).clear();
+        chromeDriver.findElement(By.cssSelector("body > div > div > form > div:nth-child(2) > input[type=password]")).clear();
         chromeDriver.findElement(By.cssSelector("body > div > div > form > div:nth-child(1) > input[type=text]")).sendKeys(username);
         chromeDriver.findElement(By.cssSelector("body > div > div > form > div:nth-child(2) > input[type=password]")).sendKeys(password);
         chromeDriver.findElement(By.cssSelector("body > div > div > form > div:nth-child(2) > a")).click();
+        // 这里要强制等待一下，否则会找不到 Alert 元素
+        Thread.sleep(2000);
+        // 登录完成后会跳出弹窗，点击确认
+        Alert alert = chromeDriver.switchTo().alert();
+        alert.accept();
+        Thread.sleep(1000);
         // 对登录结果进行检测，看是否到达了登录页面
         chromeDriver.findElement(By.cssSelector("#home > div > div.main-container > div.header > div.logo"));
         // 跳转结束后回到登录页面进行下一组测试
         chromeDriver.navigate().back();
+        Thread.sleep(2000);
+    }
+
+    @Order(3)
+    @ParameterizedTest
+    @CsvSource({"UserWrong1,1234","UserWrong2,1234"})
+    public void loginFail(String username, String password) throws InterruptedException {
+        // 插入数据前要清空，否则自动化会报错
+        chromeDriver.findElement(By.cssSelector("body > div > div > form > div:nth-child(1) > input[type=text]")).clear();
+        chromeDriver.findElement(By.cssSelector("body > div > div > form > div:nth-child(2) > input[type=password]")).clear();
+        chromeDriver.findElement(By.cssSelector("body > div > div > form > div:nth-child(1) > input[type=text]")).sendKeys(username);
+        chromeDriver.findElement(By.cssSelector("body > div > div > form > div:nth-child(2) > input[type=password]")).sendKeys(password);
+        chromeDriver.findElement(By.cssSelector("body > div > div > form > div:nth-child(2) > a")).click();
+        // 登录失败后，弹窗会报错，获取弹窗文本元素
+        String expected = "用户名或者密码错误,请重试!!";
+        Thread.sleep(2000);
+        Alert alert = chromeDriver.switchTo().alert();
+        String actual = alert.getText();
+        alert.accept();
+        // 断言弹窗文本是否和预期文本相同
+        Assertions.assertEquals(expected,actual);
     }
 }
