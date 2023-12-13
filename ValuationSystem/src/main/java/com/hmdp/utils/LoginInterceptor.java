57215368477ac10f2e30b85436987146f18da920
liu-jinxii <arny1371621210@163.com>
2023-12-13 15:06:42 +0800

    valuation-sys

--- ValuationSystem/src/main/java/com/hmdp/utils/LoginInterceptor.java
+++ ValuationSystem/src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -9,18 +9,18 @@ public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        // TODO 从 ThreadLocal 中获取用户, 如果没有则拦截
+        // 从 ThreadLocal 中获取用户, 如果没有则拦截
         if(UserHolder.getUser() == null) {
             response.setStatus(401);
             return false;
         }
-        // TODO 放行
+        // 放行
         return true;
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-       // TODO 将用户从 ThreadLocal 中移除
+       // 将用户从 ThreadLocal 中移除
        UserHolder.removeUser();
     }
 }
