a7704ab430497bdefc259b1cdd327a328ee54aa3
liu-jinxii <arny1371621210@163.com>
2023-12-14 14:59:09 +0800

    mycnblog-sys

--- MyBlogSystem-III/src/main/java/com/example/demo/service/IBlogService.java
+++ MyBlogSystem-III/src/main/java/com/example/demo/service/IBlogService.java
@@ -6,9 +6,10 @@ import com.example.demo.entity.Blog;
 import javax.servlet.http.HttpServletRequest;
 
 public interface IBlogService extends IService<Blog> {
-    public Object viewBlog(HttpServletRequest request, Integer blogId);
 
-    public Object initViews(Integer blogId);
+    Object viewBlog(HttpServletRequest request, Integer blogId);
 
-    public Object initTotalPage(Integer pageSize);
+    Object initViews(Integer blogId);
+
+    Object initTotalPage(Integer pageSize);
 }
--- ValuationSystem/src/main/java/com/hmdp/utils/RefreshTokenInterceptor.java
+++ ValuationSystem/src/main/java/com/hmdp/utils/RefreshTokenInterceptor.java
@@ -19,7 +19,7 @@ public class RefreshTokenInterceptor implements HandlerInterceptor {
     }
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        // TODO 获取到请求头中的 token
+        //  获取到请求头中的 token
         String token = request.getHeader(SystemConstants.USER_LOGIN_TOKEN);
 
         // System.out.println("RefreshToken:" + token); //
@@ -27,31 +27,31 @@ public class RefreshTokenInterceptor implements HandlerInterceptor {
         if(StrUtil.isBlank(token)) {
            return true; // 直接放行
         }
-        // TODO 基于 token 获取 redis 中的用户
+        //  基于 token 获取 redis 中的用户
         String tokenKey = RedisConstants.LOGIN_USER_KEY + token;
         Map<Object,Object> userMap = stringRedisTemplate.opsForHash()
                 .entries(tokenKey);
-        // TODO 判断用户是否存在
+        //  判断用户是否存在
         if(userMap.isEmpty()) {
             return true; // 直接放行
         }
-        // TODO 将查询到的 Hash 数据转化为 UserDTO 对象
+        //  将查询到的 Hash 数据转化为 UserDTO 对象
         UserDTO dto = BeanUtil.fillBeanWithMap(userMap, new UserDTO(), false);
 
-        // TODO 将用户信息保存到 ThreadLocal
+        //  将用户信息保存到 ThreadLocal
         UserHolder.saveUser(dto);
 
-        // TODO 刷新 token 有效期
+        //  刷新 token 有效期
         stringRedisTemplate.expire(tokenKey, RedisConstants.LOGIN_USER_TTL, TimeUnit.MINUTES);
 
-        // TODO 放行
+        //  放行
         return true;
     }
 
-    // NOTE 该方法在请求处理完之后被调用, 无论是正常还是异常现象
+    /* 该方法在请求处理完之后被调用, 无论是正常还是异常现象 */
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        // TODO 将用户从 ThreadLocal 中移除
+        //  将用户从 ThreadLocal 中移除
         UserHolder.removeUser();
     }
 }
