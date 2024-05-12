3cd3796a2ca6b4a7605ba4002fc6035cc215c4f2
liu-jinxii <arny1371621210@163.com>
2024-05-12 20:45:27 +0800

    mycnblog-sys

--- MyBlogSystem-III(Deployed)/src/main/resources/dp.sql
+++ MyBlogSystem-III(Deployed)/src/main/resources/dp.sql
@@ -7,7 +7,6 @@ create database mycnblog_plus DEFAULT CHARACTER SET utf8mb4;
 use mycnblog_plus
 
 -- 创建用户表
-
 drop table if exists user_info;
 create table user_info(
      id int primary key auto_increment,
