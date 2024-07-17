04a3fcc5c6dd46f29c8bf5f266437b3e59b25a63
liu-jinxii <arny1371621210@163.com>
2024-07-17 12:19:23 +0800

    mycnblog-sys

--- MyBlogSystem-III(Deployed)/vue/vue.config.js
+++ MyBlogSystem-III(Deployed)/vue/vue.config.js
@@ -1,10 +1,9 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  transpileDependencies: true,
-  lintOnSave : false, // 关闭 ES6 校验
+    transpileDependencies: true,
+    lintOnSave: false, // 关闭 ESLint 校验
+    assetsDir: 'static', // 静态资源目录
+    parallel: false, // 是否使用多进程构建
+    publicPath: './', // 公共路径，通常设置为根目录或相对路径
+});
 
-  /* vue项目打包配置文件 */
-  assetsDir: 'static',
-  parallel: false,
-  publicPath: './'
-})
