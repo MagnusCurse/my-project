a8aa44732f88e50051542d491fd981f42c5d5564
liu-jinxii <arny1371621210@163.com>
2024-08-12 14:05:09 +0800

    Optimize thump-up blog

--- MyBlogSystem-III(Deployed)/src/main/resources/dp.sql
+++ MyBlogSystem-III(Deployed)/src/main/resources/dp.sql
@@ -1,84 +1,71 @@
--- 创建数据库
-
+-- Create database
 drop database if exists mycnblog;
 create database mycnblog DEFAULT CHARACTER SET utf8mb4;
 
--- 使用数据数据
+-- Use the database
 use mycnblog
 
--- 创建用户表
+-- Create user table
 drop table if exists user_info;
 create table user_info(
-     id int primary key auto_increment,
-     username varchar(100) unique,
-     password varchar(64) not null,
-     photo varchar(500) default '',
-     create_time datetime default now(),
-     update_time datetime default now(),
-     mail varchar(100) unique,
-     nickname varchar(100) unique,
-     introduction varchar(500),
-     avatar_url varchar(200) default ''
+      id int primary key auto_increment,
+      username varchar(100) unique,
+      nickname varchar(100) unique,
+      mail varchar(100) unique, --
+      introduction varchar(500), -- user brief introduction
+      photo varchar(500) default '', --
+      avatar_url varchar(200) default '' -- the url of the avatar photo source
+      password varchar(64) not null,
+      create_time datetime default now(),
+      update_time datetime default now(),
 ) default charset 'utf8mb4';
 
--- 创建博客表
 
+-- Create blog table
 drop table if exists blog_info;
 create table blog_info(
-    id int primary key auto_increment,
-    title varchar(100) not null,
-    content text not null,
-    create_time datetime default now(),
-    update_time datetime default now(),
-    user_id int not null,
-    like_count int default 0,
-    view_count int default 0
+      id int primary key auto_increment, -- auto_increment_id
+      user_id int not null,
+      title varchar(100) not null,
+      content text not null,
+      like_count int default 0,
+      view_count int default 0
+      create_time datetime default now(),
+      update_time datetime default now(),
 )default charset 'utf8mb4';
 
--- 创建评论表
 
+-- Create comment table
 drop table if exists comment_info;
 create table comment_info(
-    id int primary key auto_increment,
-    parent_id int,
-    user_id int,
-    blog_id int,
-    username varchar(100),
-    comment varchar(360),
-    create_time datetime default now(),
-    like_count int,
-    replied_username varchar(100)
+     id int primary key auto_increment,
+     parent_id int,
+     user_id int,
+     blog_id int,
+     username varchar(100),
+     comment varchar(360),
+     create_time datetime default now(),
+     like_count int,
+     replied_username varchar(100)
 )default charset 'utf8mb4';
 
--- 创建点赞记录表
 
+-- Create blog thump-up record table
 drop table if exists blog_like_info;
 create table blog_like_info(
-    id int primary key auto_increment,
-    blog_id int not null, -- 被点赞博客的 id
-    user_id int not null, -- 点赞该博客用户的 id
-    create_time datetime default now(),
-    update_time datetime default now()
+   id int primary key auto_increment, -- auto_increment id
+   blog_id int not null, -- ID of the blog being liked
+   user_id int not null, -- ID of the user liking the blog
+   create_time datetime default now(),
+   update_time datetime default now()
 )default charset 'utf8mb4';
 
--- 创建关注记录表
-
+-- Create followed record table
 drop table if exists follow_info;
 create table follow_info (
-    id int primary key auto_increment,
-    user_id int not null,
-    followed_user_id int not null,
-    create_time datetime default now(),
-    update_time datetime default now()
+     id int primary key auto_increment,
+     user_id int not null, --
+     followed_user_id int not null, --
+     create_time datetime default now(),
+     update_time datetime default now()
 )default charset 'utf8mb4';
-
-
-
-
-
-
-
-
-
-
-
--- MyBlogSystem-III(Deployed)/src/test/java/com/example/demo/DemoApplicationTests.java
+++ MyBlogSystem-III(Deployed)/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,11 +1,15 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.DigestUtils;
 
 @SpringBootTest
 class DemoApplicationTests {
+	@Autowired
+	RedisTemplate redisTemplate;
 
 	@Test
 	void contextLoads() {
@@ -17,5 +21,13 @@ class DemoApplicationTests {
 		String p2 = DigestUtils.md5DigestAsHex("123".getBytes());
 		System.out.println(p1);
 		System.out.println(p2);
+	}
+
+	@Test
+	void bitMapRedisOps(String blogId, Long userId) {
+		// like the blog, set the position true
+		redisTemplate.opsForValue().setBit("blog:liked:" + blogId, userId, true);
+		// unlike the blog, set the position false
+		redisTemplate.opsForValue().setBit("blog:liked:" + blogId, userId, false);
 	}
 }
