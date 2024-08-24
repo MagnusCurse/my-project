ae12ec815dce593c169ba53d253aae30ed3f2b07
liu-jinxii <arny1371621210@163.com>
2024-08-24 14:47:39 +0800

    JWT User Entity

---
+++ JWTAuthcenticationSecurity/src/main/java/com/example/jwtauthcenticationsecurity/JwtAuthcenticationSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtauthcenticationsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthcenticationSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtAuthcenticationSecurityApplication.class, args);
+    }
+
+}
---
+++ JWTAuthcenticationSecurity/src/test/java/com/example/jwtauthcenticationsecurity/JwtAuthcenticationSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwtauthcenticationsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthcenticationSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
