dffbce4bcc777d1c3fecf4a489540eb0742d19d4
liu-jinxii <arny1371621210@163.com>
2024-08-27 11:57:47 +0800

    JWT validate

--- JWTAuthcenticationSecurity/src/main/java/com/example/jwtauthcenticationsecurity/service/JwtService.java
+++ JWTAuthcenticationSecurity/src/main/java/com/example/jwtauthcenticationsecurity/service/JwtService.java
@@ -20,12 +20,26 @@ public class JwtService {
 
     public static final String SECRET_KEY = "527052423849336f497761717263546265747741435363365a4f516a324c6d62";
 
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
     public String extractUsername(String token) {
-        // Claims::getSubject == Function<Claims, String>, :: is a simplified Lambda
         return extractClaim(token, Claims::getSubject);
     }
 
-    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+    public Date extractExpiration(String token) {
+        // `Claims::getExpiration` is a method reference that refers to the `getExpiration()` method in the `Claims` class
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+
+    public <R> R extractClaim(String token, Function<Claims, R> claimsResolver) {
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
